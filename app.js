febe3f713d8ebf7b2d0133eb2525dbef871f9475
thisyearnofear <74209106+thisyearnofear@users.noreply.github.com>
2023-05-11 15:14:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,56 +35,87 @@ let chart = new Chart(habitChart, {
         },
         beginAtZero: true
       }
-    }
+    },
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          generateLabels: function (chart) {
+            const datasets = chart.data.datasets;
+            const legendItems = chart.legend.legendItems;
+            const legendLabels = [];
+
+            datasets.forEach((dataset, i) => {
+              const label = {
+                text: dataset.label,
+                fillStyle: dataset.backgroundColor,
+                hidden: !chart.isDatasetVisible(i),
+                index: i,
+                datasetIndex: i,
+              };
+
+              if (i < activities.length) {
+                label.text += ' (Papa)';
+              } else {
+                label.text += ' (You)';
+                label.fillStyle = 'rgba(0, 0, 0, 0)';
+                label.pointStyle = 'circle';
+                label.borderWidth = 1;
+              }
+
+              legendItems[i] = label;
+              legendLabels.push(label);
+            });
+
+            return legendLabels;
+          },
+          font: {
+            size: 12,
+          },
+          padding: 10,
+          boxWidth: 12,
+          usePointStyle: true,
+        },
+      },
+    },
   }
 });
 
 document.querySelectorAll('.form-control').forEach(item => {
   item.addEventListener('input', updateChartRealTime);
+  item.addEventListener('input', validateInput);
 });
 
-function updateChartRealTime() {
-  const userInputData = [
-    { name: "Chess", days: document.getElementById('chessDays').value, hours: document.getElementById('chessHours').value },
-    { name: "Duolingo", days: document.getElementById('duolingoDays').value, hours: document.getElementById('duolingoHours').value },
-    { name: "Strava", days: document.getElementById('stravaDays').value, hours: document.getElementById('stravaHours').value },
-    { name: "Yousician", days: document.getElementById('yousicianDays').value, hours: document.getElementById('yousicianHours').value },
-  ];
+function validateInput() {
+  const maxDays = parseInt(this.getAttribute('max'));
+  const maxHours = parseInt(this.nextElementSibling.getAttribute('max'));
+  if (parseInt(this.value) > maxDays) {
+    this.value = maxDays;
+  }
+  if (parseInt(this.nextElementSibling.value) > maxHours) {
+    this.nextElementSibling.value = maxHours;
+  }
+}
 
-  chart.data.datasets.forEach((dataset, i) => {
-    if (i >= activities.length) { // Clear and update only user input datasets
-      dataset.data = []; // Clear existing data
-      const userInput = userInputData[i - activities.length]; // Get the corresponding user input
-      if (userInput.days && userInput.hours) { // Check if both fields are filled
-        dataset.data.push({ x: parseInt(userInput.days), y: parseInt(userInput.hours) });
-      }
+function updateChartRealTime() { 
+const userInputData = [
+  { name: "Chess", days: document.getElementById('chessDays').value, hours: document.getElementById('chessHours').value },
+  { name: "Duolingo", days: document.getElementById('duolingoDays').value, hours: document.getElementById('duolingoHours').value },
+  { name: "Strava", days: document.getElementById('stravaDays').value, hours: document.getElementById('stravaHours').value },
+  { name: "Yousician", days: document.getElementById('yousicianDays').value, hours: document.getElementById('yousicianHours').value },
+];
+
+chart.data.datasets.forEach((dataset, i) => {
+  if (i >= activities.length) { // Clear and update only user input datasets
+    dataset.data = []; // Clear existing data
+    const userInput = userInputData[i - activities.length]; // Get the corresponding user input
+    if (userInput.days && userInput.hours) { // Check if both fields are filled
+      dataset.data.push({ x: parseInt(userInput.days), y: parseInt(userInput.hours) });
     }
-  });
+  }
+});
 
-  // Update legend labels
-  chart.options.plugins.legend.labels.generateLabels = function (chart) {
-    const datasets = chart.data.datasets;
-    const legendItems = chart.legend.legendItems;
-    const legendLabels = [];
-
-    datasets.forEach((dataset, i) => {
-      const label = {
-        text: `${dataset.label} (${i < activities.length ? 'Papa' : 'You'})`,
-        fillStyle: dataset.backgroundColor,
-        strokeStyle: dataset.borderColor,
-        lineWidth: dataset.borderWidth,
-        hidden: !chart.isDatasetVisible(i),
-        index: i,
-        datasetIndex: i,
-      };
-      legendItems[i] = label;
-      legendLabels.push(label);
-    });
-
-    return legendLabels;
-  };
-
-  chart.update();
+chart.update();
 }
 
 window.onload = function() {
