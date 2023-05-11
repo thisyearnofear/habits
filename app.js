a15c35fcd9e78b9afa8e9cb213e34a8f9e7f2f9b
thisyearnofear <74209106+thisyearnofear@users.noreply.github.com>
2023-05-11 22:30:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,129 +1,131 @@
 window.onload = function() {
+  const activities = [
+    { name: "Run", days: [], hours: [] }
+  ];
 
-const activities = [
-  { name: "Chess", days: 5, hours: 7 },
-  { name: "Duolingo", days: 7, hours: 1 },
-  { name: "Strava", days: 1, hours: 1 },
-  { name: "Yousician", days: 3, hours: 1 },
-];
+  const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-const habitChart = document.getElementById("habitChart").getContext("2d");
+  const habitChart = document.getElementById("habitChart").getContext("2d");
 
-let chart = new Chart(habitChart, {
-  type: 'scatter',
-  data: {
-    datasets: [
-      ...activities.map((activity, i) => ({
-        label: activity.name + ' (Papa)',
-        data: [{ x: activity.days, y: activity.hours }],
-        backgroundColor: `hsl(${i * 90}, 70%, 50%)`,
-        borderColor: `hsl(${i * 90}, 70%, 50%)`,
-      })),
-      ...activities.map((activity, i) => ({
-        label: activity.name + ' (You)',
-        data: [],
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: `hsl(${i * 90}, 70%, 50%)`,
-        borderWidth: 1,
-        pointStyle: 'circle',
-      })),
-    ],
-  },
+  let chart = new Chart(habitChart, {
+    type: 'scatter',
+    data: {
+      datasets: [
+        ...activities.map((activity, i) => ({
+          label: activity.name + ' (Papa)',
+          data: [],
+          backgroundColor: `rgba(54, 162, 235, 0.5)`,
+          borderColor: `rgba(54, 162, 235, 1)`,
+        })),
+        ...activities.map((activity, i) => ({
+          label: activity.name + ' (You)',
+          data: [],
+          backgroundColor: 'rgba(255, 206, 86, 0.5)',
+          borderColor: 'rgba(255, 206, 86, 1)',
+          borderWidth: 1,
+          pointStyle: 'circle',
+        })),
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: 'category',
+          labels: daysOfWeek,
+        },
+        y: {
+          beginAtZero: true,
+          max: 24,
+        },
+      },
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            generateLabels: function (chart) {
+              const datasets = chart.data.datasets;
+              const legendItems = chart.legend.legendItems || [];
+              const legendLabels = [];
 
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          generateLabels: function (chart) {
-  const datasets = chart.data.datasets;
-  const legendItems = chart.legend.legendItems || [];
-  const legendLabels = [];
+              datasets.forEach((dataset, i) => {
+                const label = {
+                  text: dataset.label,
+                  fillStyle: dataset.backgroundColor,
+                  hidden: !chart.isDatasetVisible(i),
+                  index: i,
+                  datasetIndex: i,
+                };
 
-  datasets.forEach((dataset, i) => {
-    const label = {
-      text: dataset.label,
-      fillStyle: dataset.backgroundColor,
-      hidden: !chart.isDatasetVisible(i),
-      index: i,
-      datasetIndex: i,
-    };
+                if (i >= activities.length) {
+                  label.fillStyle = 'rgba(0, 0, 0, 0)';
+                  label.pointStyle = 'circle';
+                  label.borderWidth = 1;
+                }
 
-    if (i >= activities.length) {
-      label.fillStyle = 'rgba(0, 0, 0, 0)';
-      label.pointStyle = 'circle';
-      label.borderWidth = 1;
-    }
+                if (legendItems[i]) {
+                  legendItems[i].text = label.text;
+                  legendItems[i].fillStyle = label.fillStyle;
+                  legendItems[i].hidden = label.hidden;
+                } else {
+                  legendItems[i] = label;
+                }
 
-    if (legendItems[i]) {
-      legendItems[i].text = label.text;
-      legendItems[i].fillStyle = label.fillStyle;
-      legendItems[i].hidden = label.hidden;
-    } else {
-      legendItems[i] = label;
-    }
+                legendLabels.push(legendItems[i]);
+              });
 
-    legendLabels.push(legendItems[i]);
-  });
-
-  return legendLabels;
-}
+              return legendLabels;
+            },
+            font: {
+              size: 12,
+            },
+            padding: 10,
+            boxWidth: 12,
+            usePointStyle: true,
           },
-          font: {
-            size: 12,
-          },
-          padding: 10,
-          boxWidth: 12,
-          usePointStyle: true,
         },
       },
     },
-  },
-});
-
-document.querySelectorAll('.form-control').forEach(item => {
-  item.addEventListener('input', updateChartRealTime);
-  item.addEventListener('input', validateInput);
-});
-
-function validateInput() {
-  const maxDays = parseInt(this.getAttribute('max'));
-  let maxHours;
-  if (this.nextElementSibling) {
-    maxHours = parseInt(this.nextElementSibling.getAttribute('max'));
-  } else {
-    maxHours = parseInt(this.getAttribute('max'));
-  }
-  if (parseInt(this.value) > maxDays) {
-    this.value = maxDays;
-  }
-  if (this.nextElementSibling && parseInt(this.nextElementSibling.value) > maxHours) {
-    this.nextElementSibling.value = maxHours;
-  }
-}
-
-
-
-function updateChartRealTime() {
-  const userInputData = [
-    { name: "Chess", days: document.getElementById('chessDays').value, hours: document.getElementById('chessHours').value },
-    { name: "Duolingo", days: document.getElementById('duolingoDays').value, hours: document.getElementById('duolingoHours').value },
-    { name: "Strava", days: document.getElementById('stravaDays').value, hours: document.getElementById('stravaHours').value },
-    { name: "Yousician", days: document.getElementById('yousicianDays').value, hours: document.getElementById('yousicianHours').value },
-  ];
-
-  chart.data.datasets.forEach((dataset, i) => {    
-    if (i >= activities.length) { // Clear and update only user input datasets
-      dataset.data = []; // Clear existing data
-      const userInput = userInputData[i - activities.length]; // Get the corresponding user input
-      if (userInput.days && userInput.hours) { // Check if both fields are filled
-        dataset.data.push({ x: parseInt(userInput.days), y: parseInt(userInput.hours) });
-      }
-    }
   });
 
-  chart.update();
-}
+  function updateChartRealTime() {
+    const userInputData = [
+      { name: "Run", hours: [] },
+    ];
+
+    daysOfWeek.forEach((day, i) => {
+      const inputElement = document.getElementById(day.toLowerCase());
+      const inputValue = inputElement.value.trim();
+
+      const activityData = {
+        x: day,
+        y: inputValue !== "" ? parseInt(inputValue) : null
+      };
+
+      userInputData[0].hours.push(activityData);
+    });
+
+    chart.data.datasets.forEach((dataset, i) => {
+      if (i >= activities.length) {
+        dataset.data = userInputData[i - activities.length].hours;
+      }
+    });
+
+    chart.update();
+  }
+
+  document.querySelectorAll('.form-control').forEach(item => {
+    item.addEventListener('input', updateChartRealTime);
+    item.addEventListener('input', validateInput);
+  });
+
+  function validateInput() {
+    const inputValue = parseInt(this.value);
+    const maxHours = 24;
+
+    if (isNaN(inputValue) || inputValue < 0 || inputValue > maxHours) {
+      this.value = "";
+    }
+  }
 
   updateChartRealTime();
-
 }
